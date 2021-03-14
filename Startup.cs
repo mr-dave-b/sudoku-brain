aa2d6e899dc6657c89375aac0aa36f64ba0662bf
David Brooks <ms@davebeta.com>
2021-03-14 00:09:37 +0000

    Added application insights

--- Startup.cs
+++ Startup.cs
@@ -18,6 +18,8 @@ namespace sudoku_brain
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry();
+            
             services.AddControllersWithViews();
         }
 
